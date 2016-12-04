91745a36ff5c088d0611414791f66493406c8ee5
Daniela Rodríguez Careri <dcareri@gmail.com>
2016-12-04 19:35:24 -0300

    Documentation and config files

--- Radio_Dani/Radio_Dani.ino
+++ Radio_Dani/Radio_Dani.ino
@@ -1,3 +1,12 @@
+/**
+ * Multi radio
+ * Daniela Rodríguez Careri <dcareri@gmail.com>
+ *  
+ * @todo Initialize with sim values at startup
+ * @todo Show which digits will the encoder affect
+ * @todo Show message while X-Plane is connecting because the device is unresponsive until then
+ * @todo Add schematics to repo 
+ */
 #include <ArdSim_Interface.h>
 #include <LiquidCrystal.h>
 #include <Encoder.h>
